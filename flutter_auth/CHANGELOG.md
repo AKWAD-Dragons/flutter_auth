ce9f92a9eea79a9f0812fb77a90b8b3a3c0a885d
Ahmed Adel <AhmedAdel94@users.noreply.github.com>
2020-08-13 11:19:09 +0200

    Fix package name conflict

--- flutter_auth/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-## 1.0.0-dev.2 - First Version (Pre-release)
-
-* TODO: Added common authentication methods
--- flutter_auth/ios/Classes/FlutterAuthPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterAuthPlugin : NSObject<FlutterPlugin>
-@end
