d962cf2472c9fd2fb73a911562abaf6166a1a388
Ahmed Adel <AhmedAdel94@users.noreply.github.com>
2021-05-02 14:47:06 +0200

    Fix issue with amazon login package on iOS

--- flutter_auth/ios/Classes/LoginWithAmazonProxy.swift
+++ flutter_auth/ios/Classes/LoginWithAmazonProxy.swift
@@ -8,7 +8,7 @@
 //  https://gist.github.com/mobilequickie/56916503a41ebb2374fea241ede26eab
 
 import Foundation
-import LoginWithAmazon
+
 
 class LoginWithAmazonProxy {
     
@@ -16,29 +16,29 @@ class LoginWithAmazonProxy {
     
     // The result of this API requestDidSucceed: callback for a successful login OR
     // sent to requestDidFail: callback if the login failed
-    func login(delegate: AIAuthenticationDelegate) {
-        AIMobileLib.authorizeUser(forScopes: ["profile", "postal_code"], delegate: delegate)
+    func login() {
+        //AIMobileLib.authorizeUser(forScopes: ["profile", "postal_code"], delegate: delegate)
     }
     
-    func logout(delegate: AIAuthenticationDelegate) {
-        AMZNAuthorizationManager.shared().signOut { (error) in
-            if((error) != nil) {
-                print("error signing out from LWA: \(String(describing: error))")
-            }
-            // Deletes cached user tokens and other data from the keychain.
-            // .signOut should be doing this, redundant?
-            AIMobileLib.clearAuthorizationState(delegate)
-        }
+    func logout() {
+        // AMZNAuthorizationManager.shared().signOut { (error) in
+        //     if((error) != nil) {
+        //         print("error signing out from LWA: \(String(describing: error))")
+        //     }
+        //     // Deletes cached user tokens and other data from the keychain.
+        //     // .signOut should be doing this, redundant?
+        //     AIMobileLib.clearAuthorizationState(delegate)
+        // }
     }
     
     // The result of this API is a new access token sent to requestDidSucceed: callback
-    func getAccessToken(delegate: AIAuthenticationDelegate) {
-        AIMobileLib.getAccessToken(forScopes: ["profile", "postal_code"], withOverrideParams: nil, delegate: delegate)
+    func getAccessToken() {
+        //AIMobileLib.getAccessToken(forScopes: ["profile", "postal_code"], withOverrideParams: nil, delegate: delegate)
     }
 
     // The result of this API is a user profile as an NSDictionary sent to requestDidSucceed: callback
     // e.g. email = "user@domain.com";name = "first last"; "postal_code" = "xxxxx"; "user_id" = "amzn1.account.xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    func getUserProfile(delegate: AIAuthenticationDelegate) {
-        AIMobileLib.getProfile(delegate)
+    func getUserProfile() {
+        //AIMobileLib.getProfile(delegate)
     }
 }
